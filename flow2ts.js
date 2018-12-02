e790f97252b44c168ceaf77efdb9ac1a827fb429
sizer <shinse.tanaka@gmail.com>
2018-12-02 18:03:25 +0900

    create dir when target dir is not exist.

--- flow2ts.js
+++ flow2ts.js
@@ -1,6 +1,7 @@
 const flow2typescript = require('./flow-to-typescript/dist/src')
 const fs = require('fs')
 const glob = require('glob')
+const path = require('path')
 
 const convertJS2TS = path => path.replace('\.js', '\.ts').replace('src', 'ts')
 
@@ -8,9 +9,12 @@ const compile2TS = (file) => (
   flow2typescript.compile(
     fs.readFileSync(file, 'utf-8'),
     file,
-  ).then(ts => 
+  ).then(ts => {
+    fs.promises.mkdir(path.dirname(convertJS2TS(file)), { recursive: true })
+    return ts
+  }).then(ts => {
     fs.writeFileSync(convertJS2TS(file), ts)
-  )
+  })
 )
 
 let pattern = 'src/**/*.js*'
